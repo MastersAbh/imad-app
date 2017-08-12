58a02b90e9bbe8f9a67e6ba603828cb3e8480c81
MastersAbh <mastersabhinav@gmail.com>
2017-08-12 19:57:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,68 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+
+var artone={
+    title: 'Art 1'
+    , heading: 'Article One',
+    date: '12 Aug',
+    content: `
+        Content Should Display Here
+    
+    `
+};
+
+
+
+function createtemp(data){
+    var title=data.title;
+    var head=data.heading;
+    var cont=data.content;
+    var date=data.date;
+    
+    
+    var temp=`<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device-width,initial scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${head}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                <p>
+                    ${cont}
+                </p>
+                <p>
+                    The Article is here
+                </p>
+                <p>
+                    The start is here
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
+
+
+
+    `
+    return temp;
+}
+
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -13,7 +75,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/art-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'art-one.html'));
+   res.send(createtemp(artone));
 });
 app.get('/art-two', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'art-two.html'));
