e7d707eec7934860707322d793f8e01ad26b63cf
MastersAbh <mastersabhinav@gmail.com>
2017-08-12 21:46:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 alert ("Hi I am Abhinav");
 var ele=document.getElementById('me');
 ele.innerHTML='New Val';
+
+var im= document.getElementById('ma');
+im.onclick = function() {
+  im.style.marginLeft='100px';  
+};
