b713145c9d37c07b9ec54d43a4876719833a702e
MastersAbh <mastersabhinav@gmail.com>
2017-08-12 19:01:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/art-one', function (req, res) {
+  res.send('Art-one is served here');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
