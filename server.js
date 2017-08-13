1b1307148fb8df10b282fbe713c76ba72d471395
MastersAbh <mastersabhinav@gmail.com>
2017-08-13 12:24:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,14 @@ function createtemp(data){
 }
 
 
+
+var counter=0;
+
+app.get('/counter', function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
+
 var app = express();
 app.use(morgan('combined'));
 
