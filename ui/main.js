6ae7c27203fde9629a63d9cb2e1a1b0c4d4d6fe5
MastersAbh <mastersabhinav@gmail.com>
2017-08-13 22:21:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,29 @@ button.onclick = function() {
     request.open('GET', 'http://mastersabhinav.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var input=document.getElementById('name');
+var name=input.value;
+var submit=document.getElementById('sub');
+
+submit.onclick= function() {
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names= request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+= '<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    
+    request.open('GET', 'http://mastersabhinav.imad.hasura-app.io/sub?name='+name, true);
+    request.send(null);
+    
+}
