cfbea8a526803ab4e31cbf5e0aaca25d941ef40a
MastersAbh <mastersabhinav@gmail.com>
2017-08-16 01:37:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button = document.getElementById('counter');
+/*var button = document.getElementById('counter');
 
 button.onclick = function() {
     
@@ -18,9 +18,29 @@ button.onclick = function() {
     
     request.open('GET', 'http://mastersabhinav.imad.hasura-app.io/counter', true);
     request.send(null);
+};*/
+
+var button= document.getElementById('counter');
+button.onclick = function(){
+ 
+var request=new XMLHttpRequest();
+request.onreadystatechange= function()
+{
+    if(request.readystate === XMLHttpRequest.DONE)
+    {
+        if(request.status===200){
+        var counter=request.responseText;
+        var span= document.getElementById('count');
+        span.innerHTML=counter.toString();
+    }
+    }
 };
+ 
+ request.open('GET', 'http://mastersabhinav.imad.hasura-app.io/counter',true);
+ request.send(null);
+ 
 
-
+};
 
 var submit=document.getElementById('sub');
 
