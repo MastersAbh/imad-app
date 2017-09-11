5b1aacf351df91669cfa9d2949acfe92436b2464
MastersAbh <mastersabhinav@gmail.com>
2017-09-11 12:00:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,29 +20,9 @@ button.onclick = function() {
     request.send(null);
 };*/
 
-var button= document.getElementById('counter');
-button.onclick = function(){
- 
-var request=new XMLHttpRequest();
-request.onreadystatechange= function()
-{
-    if(request.readyState === XMLHttpRequest.DONE)
-    {
-        if(request.status===200){
-        var counter=request.responseText;
-        var span= document.getElementById('count');
-        span.innerHTML=counter.toString();
-    }
-    }
-};
- 
- request.open('GET', 'http://mastersabhinav.imad.hasura-app.io/counter',true);
- request.send(null);
- 
 
-};
 
-var submit=document.getElementById('sub');
+var submit=document.getElementById('submit_btn');
 
 submit.onclick= function() {
     var input=document.getElementById('name');
@@ -51,19 +31,17 @@ var name=input.value;
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var names= request.responseText;
-                names=JSON.parse(names);
-                var list='';
-                for(var i=0;i<names.length;i++){
-                    list+= '<li>'+names[i]+'</li>';
-                }
-                var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+                alert("Logged In");
+            }
+            else{
+                alert("Incorrect");
             }
         }
     };
-    
-    request.open('GET', 'http://mastersabhinav.imad.hasura-app.io/sub?name='+name, true);
-    request.send(null);
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    request.open('POST', 'http://mastersabhinav.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
 }
