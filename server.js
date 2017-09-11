05e61dd7d3430ea52980148e4ce08a6736aa73f7
MastersAbh <mastersabhinav@gmail.com>
2017-09-11 11:30:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ var app = express();
 
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var config = {
     user: 'mastersabhinav',
     database: 'mastersabhinav',
@@ -90,6 +91,7 @@ var config = {
     password: process.env.DB_PASSWORD
 }
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -115,6 +117,21 @@ app.get('/hash/:input', function(req,res){
    res.send(hashedString); 
 });
 
+app.post('/create-user', function(req,res){
+    var username= req.body.username;
+    var password= req.body.password;
+    
+    var salt= crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt); 
+   pool.query('Insert into "user" (username, password) values ($1,$2)',[username, dbString], function(err,result){
+      if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           res.send('user successfully created!');
+       }
+   });
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
